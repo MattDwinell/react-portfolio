5c5c11da6d33ec5de65e98d295cc1625185d674c
MattDwinell <dwinell.matt@gmail.com>
2021-03-20 15:04:21 -0400

    finish about skeleton, begin footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Welcome from './components/Welcome'
 import ProjectPage from './components/ProjectPage'
 import About from './components/AboutPage'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="container">
         <Welcome/>
         <ProjectPage />
         <About/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+             &copy; Matthew Dwinell,  {new Date().getFullYear()}
+        </footer>
+    )
+}
+
+export default Footer
